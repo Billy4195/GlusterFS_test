f9ff4e39a59804db6a81b9255a3e90407d2ae399
Billy.Su <g4691821@gmail.com>
2016-08-26 02:13:39 +0800

    auto copy ssh and add hosts to cluster

--- key_set.sh
+++ key_set.sh
@@ -1,9 +1,21 @@
 #!/bin/bash 
 
-ssh-copy-id root@vm94
-ssh-copy-id root@vm95
-ssh-copy-id root@vm96
+hosts=($(cat /etc/hosts | grep -v local | awk {' print $2 '} | grep -v backup ))
+add=$(tail -n 1 /etc/hosts)
+match=$(echo $add | awk {'print $2'})
+for ((index=0;index<${#hosts[@]};index++))
+do
+    ssh-copy-id root@${hosts[$index]]} 
+    if [ $? -eq 0 ]
+    then
+        echo "${hosts[$index]} copy key success!!"
+    fi
+    tmp=$(ssh root@${hosts[$index]} "cat /etc/hosts | grep $match")
+    #echo $tmp
+    if [ $? -ne 0 ]
+    then
+        ssh root@${hosts[$index]} "echo '$add' >> /etc/hosts"
+    fi
+    echo ""
+done
 
-ssh root@vm94 echo "VM94 copy key success"
-ssh root@vm95 echo "VM95 copy key success"
-ssh root@vm96 echo "VM96 copy key success"
