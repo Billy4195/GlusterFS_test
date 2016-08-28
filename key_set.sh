ada4332693440a310f3cb99e9c560e693bac303c
Billy.Su <g4691821@gmail.com>
2016-08-28 21:26:29 +0800

    tmux install

--- key_set.sh
+++ key_set.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
+rpm -qa | grep tmux >/dev/null
+if [ $? -ne 0 ]
+then
+    yum install -y tmux
+fi
+
 hosts=($(cat /etc/hosts | grep -v local | awk {' print $2 '} | grep -v backup ))
 add=$(tail -n 1 /etc/hosts)
 match=$(echo $add | awk {'print $2'})
 for ((index=0;index<${#hosts[@]};index++))
 do
-    ssh-copy-id root@${hosts[$index]]} 
+    ssh-copy-id root@${hosts[$index]]}  2>/dev/null
     if [ $? -eq 0 ]
     then
         echo "${hosts[$index]} copy key success!!"
